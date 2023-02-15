97d0da6541d87018ff663ccf8a4723d654c47d43
dwiest <dw9494@gmail.com>
2023-02-15 19:18:47 +0000

    Use Django settings instead of hardcoding values.

--- dwiest/django/demos/qrcode/forms.py
+++ dwiest/django/demos/qrcode/forms.py
@@ -2,25 +2,27 @@ from django import forms
 import base64
 import qrcode
 from io import BytesIO
+from ..conf import settings
 
 class QrcodeForm(forms.Form):
   input_text = forms.CharField(
     label='input_text',
-    initial='')
+    initial=settings.DEMOS_QRCODE_INITIAL_TEXT,
+    )
 
   def get_qrcode(self):
     #data = "https://wiest.world/"
     qr = qrcode.QRCode(
-      version=1,
-      error_correction=qrcode.constants.ERROR_CORRECT_H,
-      box_size=10,
-      border=4
+      version=settings.DEMOS_QRCODE_VERSION,
+      error_correction=settings.DEMOS_QRCODE_ERROR_CORRECTION,
+      box_size=settings.DEMOS_QRCODE_BOX_SIZE,
+      border=settings.DEMOS_QRCODE_BORDER,
       )
     qr.add_data(self.cleaned_data['input_text'])
     qr.make(fit=True)
-    img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
+    img = qr.make_image(fill_color=settings.DEMOS_QRCODE_FILL_COLOR, back_color=settings.DEMOS_QRCODE_BACKGROUND_COLOR).convert('RGB')
     #img.save("static/qrcode.png")
     stream = BytesIO()
-    img.save(stream, format="PNG")
+    img.save(stream, format=settings.DEMOS_QRCODE_FORMAT)
     encoded_img = base64.b64encode(stream.getvalue()).decode("utf-8")
     return encoded_img
