68b17c6273f7db6836216a862e339b1a88d911d6
dwiest <dw9494@gmail.com>
2023-02-16 02:16:45 +0000

    Set the widget attribute on SeleniumForm's url field so we can resize it.
    Uppercase the labels for the width and height fields.
    
    Added width and height keyword arguments to SeleniumForm's constructor
    to allow initial field values to be specified.
    
    Moved the super() call to the top of SeleniumForm's constructor.  This
    needs to run to setup the fields attribute on the form.

--- dwiest/django/demos/selenium/forms.py
+++ dwiest/django/demos/selenium/forms.py
@@ -7,24 +7,32 @@ class SeleniumForm(forms.Form):
   url = forms.URLField(
     label='url',
     initial=settings.DEMOS_SELENIUM_INITIAL_URL,
+    widget=forms.TextInput(attrs={'size': settings.DEMOS_SELENIUM_INPUT_SIZE}),
     )
 
   width = forms.IntegerField(
-    label='width',
+    label='Width',
     initial=settings.DEMOS_SELENIUM_IMAGE_WIDTH_INITIAL,
     min_value=settings.DEMOS_SELENIUM_IMAGE_WIDTH_MIN,
     max_value=settings.DEMOS_SELENIUM_IMAGE_WIDTH_MAX,
     )
 
   height = forms.IntegerField(
-    label='height',
+    label='Height',
     initial=settings.DEMOS_SELENIUM_IMAGE_HEIGHT_INITIAL,
     min_value=settings.DEMOS_SELENIUM_IMAGE_HEIGHT_MIN,
     max_value=settings.DEMOS_SELENIUM_IMAGE_HEIGHT_MAX,
     )
 
-  def __init__(self, user, *args, **kwargs):
+  def __init__(self, user, width=None, height=None, *args, **kwargs):
+    super(forms.Form, self).__init__(*args, **kwargs)
     self.user = user
+
+    if width:
+      self.fields['width'].initial = width
+    if height:
+      self.fields['height'].initial = height
+
     options = webdriver.ChromeOptions()
 
     options.add_argument("--headless")
@@ -38,8 +46,6 @@ class SeleniumForm(forms.Form):
       settings.DEMOS_SELENIUM_CHROME_DRIVER_PATH,
       chrome_options=options,
       )
-
-    super(forms.Form, self).__init__(*args, **kwargs)
 
   def screenshot(self, *args, **kwargs):
     self.browser.set_window_size(self.cleaned_data['width'],self.cleaned_data['height'])
