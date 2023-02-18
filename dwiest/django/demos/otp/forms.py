51cfb28ca2001b766c0404c02c7b4853235c486b
dwiest <dw9494@gmail.com>
2023-02-18 20:37:39 +0000

    Add an error_messages dictionary to OtpForm.
    Set an intial QR code, OTP token and secret key for an unbound form.
    
    Check that the length of the secret key is exactly 32 characters in
    OtpForm.clean_secret_key().
    
    Added an OtpForm.process() method and moved logic out of the view.
    
    Added class methods to OtpForm for:
      - get_invalid_secret_key()
      - get_provisioning_uri()
      - get_secret_key()
      - get_otp()
      - get_qrcode()

--- dwiest/django/demos/otp/forms.py
+++ dwiest/django/demos/otp/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
 import base64
 from io import BytesIO
 import pyotp
@@ -29,40 +31,91 @@ class OtpForm(forms.Form):
     widget=forms.TextInput(attrs={'class': settings.DEMOS_OTP_QRCODE_PROVISIONING_ISSUER_CLASS})
     )
 
+  error_messages = {
+    'invalid_secret_key':
+      _('The secret key is invalid, it must be 32 characters long.')
+  }
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+
+    if 'data' not in kwargs: # form not bound
+      secret_key = self.fields['secret_key'].initial
+
+      if not secret_key:
+        secret_key = OtpForm.get_secret_key()
+        self.fields['secret_key'].initial = secret_key
+
+      self.secret_key = secret_key
+      self.otp = OtpForm.get_otp(secret_key)
+
+      self.provisioning_uri = OtpForm.get_provisioning_uri(
+        self.secret_key,
+        self.fields['name'].initial,
+        self.fields['issuer_name'].initial)
+
+      self.qrcode = OtpForm.get_qrcode(self.provisioning_uri)
+
   def clean_secret_key(self):
-    if self.cleaned_data['secret_key'] == '':
-      secret_key = pyotp.random_base32()
+
+    if not self.cleaned_data['secret_key']:
+      secret_key = OtpForm.get_secret_key()
       new_data = self.data.copy()
       new_data['secret_key'] = secret_key
       self.data = new_data
       return secret_key
+
+    elif len(self.cleaned_data['secret_key']) != 32:
+        raise OtpForm.get_invalid_secret_key_error()
+
     else:
       return self.cleaned_data['secret_key']
 
-  def get_otp(self):
-    totp = pyotp.TOTP(self.cleaned_data['secret_key'])
+  def process(self):
+    self.secret_key = self.cleaned_data['secret_key']
+    name = self.cleaned_data['name']
+    issuer_name = self.cleaned_data['issuer_name']
+    self.otp = OtpForm.get_otp(self.secret_key)
+    self.provisioning_uri = OtpForm.get_provisioning_uri(self.secret_key, name, issuer_name)
+    self.qrcode = OtpForm.get_qrcode(self.provisioning_uri)
+
+  @classmethod
+  def get_invalid_secret_key_error(cls):
+    return ValidationError(
+      cls.error_messages['invalid_secret_key'],
+      code = 'invalid_secret_key')
+
+  @classmethod
+  def get_provisioning_uri(cls, secret_key, name, issuer_name):
+    return pyotp.TOTP(secret_key).provisioning_uri(name=name, issuer_name=issuer_name)
+
+  @classmethod
+  def get_secret_key(cls):
+    return pyotp.random_base32()
+
+  @classmethod
+  def get_otp(cls, secret_key):
+    totp = pyotp.TOTP(secret_key)
     return totp.now()
 
-  def get_provisioning_uri(self):
-    return pyotp.TOTP(self.cleaned_data['secret_key']).provisioning_uri(name=self.cleaned_data['name'], issuer_name=self.cleaned_data['issuer_name'])
+  @classmethod
+  def get_qrcode(cls, text):
+    qr = qrcode.QRCode(
+      version=settings.DEMOS_OTP_QRCODE_VERSION,
+      error_correction=settings.DEMOS_OTP_QRCODE_ERROR_CORRECTION,
+      box_size=settings.DEMOS_OTP_QRCODE_BOX_SIZE,
+      border=settings.DEMOS_OTP_QRCODE_BORDER
+    )
 
-def get_qrcode(text):
-  qr = qrcode.QRCode(
-    version=settings.DEMOS_OTP_QRCODE_VERSION,
-    error_correction=settings.DEMOS_OTP_QRCODE_ERROR_CORRECTION,
-    box_size=settings.DEMOS_OTP_QRCODE_BOX_SIZE,
-    border=settings.DEMOS_OTP_QRCODE_BORDER
-  )
+    qr.add_data(text)
+    qr.make(fit=True)
 
-  qr.add_data(text)
-  qr.make(fit=True)
+    img = qr.make_image(
+      fill_color=settings.DEMOS_OTP_QRCODE_FILL_COLOR,
+      back_color=settings.DEMOS_OTP_QRCODE_BACKGROUND_COLOR
+      ).convert('RGB')
 
-  img = qr.make_image(
-    fill_color=settings.DEMOS_OTP_QRCODE_FILL_COLOR,
-    back_color=settings.DEMOS_OTP_QRCODE_BACKGROUND_COLOR
-    ).convert('RGB')
-
-  stream = BytesIO()
-  img.save(stream, format=settings.DEMOS_OTP_QRCODE_FORMAT)
-  encoded_img = base64.b64encode(stream.getvalue()).decode("utf-8")
-  return encoded_img
+    stream = BytesIO()
+    img.save(stream, format=settings.DEMOS_OTP_QRCODE_FORMAT)
+    encoded_img = base64.b64encode(stream.getvalue()).decode("utf-8")
+    return encoded_img
