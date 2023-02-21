3a76a8d5c00f52ee88fbc63adb3ecf1deb1a8de6
dwiest <dw9494@gmail.com>
2023-02-21 22:59:04 +0000

    Removed LoginRequiredMixin from views. Use https://github.com/twidi/django-decorator-include instead.

--- dwiest/django/demos/urls.py
+++ dwiest/django/demos/urls.py
@@ -13,17 +13,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-
-class myTemplateView(LoginRequiredMixin, TemplateView):
-  pass
 
 app_name='demos'
 urlpatterns = [
   path('',
-    myTemplateView.as_view(
+    TemplateView.as_view(
       template_name="dwiest-django-demos/home.html"), name='home'
     ),
   path('file/',
