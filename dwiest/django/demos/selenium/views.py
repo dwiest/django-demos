28fa15f2c578c6efd7e65220b7a0f95cc2be4852
dwiest <dw9494@gmail.com>
2023-02-24 16:16:33 +0000

    Added ResponseDict Enum to replace hardcoded strings.
    Removed unused 'static_url' from response_dict.

--- dwiest/django/demos/selenium/views.py
+++ dwiest/django/demos/selenium/views.py
@@ -3,31 +3,34 @@ from django.shortcuts import render
 from django.template.context import RequestContext
 from django.views.generic import FormView
 from django.views.generic.base import TemplateResponseMixin
+from enum import Enum
 from .forms import SeleniumForm
 from ..conf import settings
 
 class SeleniumView(FormView, TemplateResponseMixin):
+
+  class ResponseDict(str, Enum):
+    FORM = 'form'
+
   form_class = SeleniumForm
   template_name = settings.DEMOS_SELENIUM_TEMPLATE
   success_url = '.'
 
   def __init__(self, *args, **kwargs):
-    self.response_dict = {
-      'static_url' : settings.STATIC_URL,
-    }
+    self.response_dict = {}
 
     return super(FormView, self).__init__(*args, **kwargs)
 
   def get(self, request, *args, **kwargs):
     form = self.form_class(request.user, data=request.GET)
-    self.response_dict['form'] = form
+    self.response_dict[self.ResponseDict.FORM] = form
     if form.is_valid():
       form.process()
     return render(request, self.template_name, self.response_dict)
 
   def post(self, request, *args, **kwargs):
     form = self.form_class(request.user, data=request.POST)
-    self.response_dict['form'] = form
+    self.response_dict[self.ResponseDict.FORM] = form
     if form.is_valid():
       form.process()
     return render(request, self.template_name, self.response_dict)
