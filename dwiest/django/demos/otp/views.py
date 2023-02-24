65fe3c5d5e158093c89561396ed40d2deccf56ac
dwiest <dw9494@gmail.com>
2023-02-24 19:33:57 +0000

    Replaced hardcoded strings with references to ResponseDict Enum.
    Removed unnecessary "else" clause.

--- dwiest/django/demos/otp/views.py
+++ dwiest/django/demos/otp/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render
 from django.template.context import RequestContext
 from django.views.generic import FormView
 from django.views.generic.base import TemplateResponseMixin
+from enum import Enum
 from .forms import OtpForm
 from ..conf import settings
 
 class OtpView(FormView, TemplateResponseMixin):
+
+  class ResponseDict(str, Enum):
+    FORM = 'form'
+
   form_class = OtpForm
   template_name = settings.DEMOS_OTP_TEMPLATE
   success_url = '.'
@@ -17,15 +22,19 @@ class OtpView(FormView, TemplateResponseMixin):
     return super(FormView, self).__init__(*args, **kwargs)
 
   def get(self, request, *args, **kwargs):
-    form = OtpForm()
-    self.response_dict['form'] = form
+    form = OtpForm(data=request.GET)
+    self.response_dict[self.ResponseDict.FORM] = form
+
+    if form.is_valid():
+      form.process()
+
     return render(request, self.template_name, self.response_dict)
 
   def post(self, request, *args, **kwargs):
     form = OtpForm(data=request.POST)
-    self.response_dict['form'] = form
+    self.response_dict[self.ResponseDict.FORM] = form
+
     if form.is_valid():
       form.process()
-      return render(request, self.template_name, self.response_dict)
-    else:
-      return render(request, self.template_name, self.response_dict)
+
+    return render(request, self.template_name, self.response_dict)
