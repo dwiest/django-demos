18a5ca0ea88c93e54bddf114413cbd9be904ac10
dwiest <dw9494@gmail.com>
2023-02-24 20:32:28 +0000

    Removed commented out lines.

--- dwiest/django/demos/file/models.py
+++ dwiest/django/demos/file/models.py
@@ -15,8 +15,6 @@ class File(models.Model):
   path = models.CharField(max_length=36)
 
   content_type = models.CharField(max_length=32)
-#  type = models.ForeignKey(
-#    'ContentType', on_delete=models.PROTECT)
 
   size = models.IntegerField() # bytes
   created_at = models.DateTimeField(auto_now_add=True)
@@ -61,50 +59,3 @@ class FileSummary(models.Model):
 
   files = models.IntegerField()
   size = models.IntegerField()
-
-#class ContentType(models.Model):
-#''' content-types, e.g. text/plain, text/html, x-application/binary-data '''
-#  name = models.CharField(max_length=256)
-
-
-#class FileExtension(models.Model):
-#''' A filename extension and its associated content-type '''
-#  type = models.ForeignKey(
-#    'ContentType', on_delete = models.PROTECT)
-#
-#  extension = models.CharField(max_length=12)
-
-
-#class FileVersion(models.Model):
-#''' A version of a file. Keeps track of its oldest sibling file '''
-#  file = models.OneToOneField(File)
-#  oldest_sibling = models.OneToOneField(File)
-#  version = models.IntegerField()
-
-
-#class Checksum(models.Model):
-#''' A type of checksum, e.g. SHA256, MD5 '''
-#  type = models.CharField(max_length=12)
-
-
-#class FileChecksum(models.Model):
-#''' A checksum for a file '''
-#  checksum = models.ForeignKey(
-#    'Checksum', on_delete=models.PROTECT)
-#
-#  file = models.ForeignKey(
-#    'File', on_delete=models.PROTECT)
-#
-#  value = models.CharField(max_length=256)
-
-
-#class UploadProcessor(models.Model):
-#''' File processors, e.g. Anti-virus scanner '''
-#  type = models.CharField()
-#
-#
-#class ProcessedFile(models.Model):
-#  processor = scan:
-#  file
-#  date_processed
-#  result
