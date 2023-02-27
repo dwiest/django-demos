102ac2299859b1eb7caf962a858f1bb70706892d
dwiest <dw9494@gmail.com>
2023-02-27 17:18:26 +0000

    Added owner as a foreign key.
    Removed __init__() that just called super().__init__()
    
    Formatting.
    
    Set editable to False for created_at field.
    
    Changed auto_now_add to auto_now for last_modified field and set editable to False.
    
    Removed auto_now_add=False from article_date field.

--- dwiest/django/demos/bookmarks/models.py
+++ dwiest/django/demos/bookmarks/models.py
@@ -1,16 +1,42 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Bookmark(models.Model):
-  url = models.CharField(max_length=255)
-  title = models.CharField(max_length=255)
-  description = models.TextField(blank=True, null=True)
-  created_at = models.DateTimeField(auto_now_add=True)
-  last_modified = models.DateTimeField(auto_now_add=True)
-  article_date = models.DateTimeField(auto_now_add=False, blank=True, null=True)
+  owner = models.ForeignKey(
+    User,
+    on_delete=models.PROTECT,
+    blank = True,
+    null = True,
+    )
 
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args,**kwargs)
+  url = models.CharField(
+    max_length=255,
+    )
+
+  title = models.CharField(
+    max_length=255,
+    )
+
+  description = models.TextField(
+    blank=True,
+    null=True,
+    )
+
+  created_at = models.DateTimeField(
+    auto_now_add=True,
+    editable=False,
+    )
+
+  last_modified = models.DateTimeField(
+    auto_now=True,
+    editable=False,
+    )
+
+  article_date = models.DateTimeField(
+    blank=True,
+    null=True,
+    )
 
 #class Screenshot(models.Model):
 #  bookmark = models.ForeignKey(Bookmark)
