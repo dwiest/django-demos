2bf4cff833e37d1e85523e52707c116a4cb0dde2
dwiest <dw9494@gmail.com>
2023-02-27 17:24:51 +0000

    Changed form_class from BookmarkForm to QuickBookmarkForm for BookmarksView.
    Get bookmarks for appropriate user in BookmarksView.get().
    
    AddBookmarkView -> QuickAddBookmarkView
    
    Changed form_class from BookmarkForm to QuickBookmarkForm for
    QuickAddBookmarkView.
    
    Added BookmarkView class to handle detailed add/edit of bookmarks.

--- dwiest/django/demos/bookmarks/views.py
+++ dwiest/django/demos/bookmarks/views.py
@@ -1,20 +1,21 @@
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, reverse
 from django.template.context import RequestContext
 from django.views.generic import FormView, TemplateView
 from django.views.generic.base import TemplateResponseMixin
 from enum import Enum
-from .forms import BookmarkForm
+from .forms import *
 from .models import Bookmark
 from ..conf import settings
 
-class BookmarksView(TemplateView):#FormView, TemplateResponseMixin):
+class BookmarksView(TemplateView):
 
   class ResponseDict(str, Enum):
     BOOKMARKS = 'bookmarks'
     FORM = 'form'
 
-  form_class = BookmarkForm
+  form_class = QuickBookmarkForm
   template_name = settings.DEMOS_BOOKMARKS_TEMPLATE
 
   def __init__(self, *args, **kwargs):
@@ -23,17 +24,21 @@ class BookmarksView(TemplateView):#FormView, TemplateResponseMixin):
 
   def get(self, request, *args, **kwargs):
     form = self.form_class()
-    bookmarks = Bookmark.objects.all()
+    if request.user and request.user.id:
+      bookmarks = Bookmark.objects.filter(owner=request.user)
+    else:
+      bookmarks = Bookmark.objects.filter(owner=None)
     self.response_dict[self.ResponseDict.BOOKMARKS] = bookmarks
     self.response_dict[self.ResponseDict.FORM] = form 
     return render(request, self.template_name, self.response_dict)
 
-class AddBookmarkView(FormView, TemplateResponseMixin):
+
+class QuickAddBookmarkView(FormView, TemplateResponseMixin):
 
   class ResponseDict(str, Enum):
     FORM = 'form'
 
-  form_class = BookmarkForm
+  form_class = QuickBookmarkForm
 
   def __init__(self, *args, **kwargs):
     self.response_dict = {}
@@ -46,3 +51,57 @@ class AddBookmarkView(FormView, TemplateResponseMixin):
       form.process()
       form.save()
     return HttpResponseRedirect(reverse('demos:bookmarks:home'))
+
+
+class BookmarkView(TemplateView):
+
+  class ResponseDict(str, Enum):
+    FORM = 'form'
+
+  form_class = BookmarkForm
+  success_page = 'demos:bookmarks:home'
+  template_name = settings.DEMOS_BOOKMARKS_BOOKMARK_TEMPLATE
+
+  def __init__(self, *args, **kwargs):
+    self.response_dict = {}
+
+  def get(self, request, *args, **kwargs):
+    bookmark_id = request.GET.get('id')
+
+    if bookmark_id:
+      try:
+        if request.user and request.user.id:
+          bookmark = Bookmark.objects.get(owner=request.user, id=bookmark_id)
+        else:
+          bookmark = Bookmark.objects.get(owner=None, id=bookmark_id)
+      except Exception as e:
+        print(str(e))
+        messages.error(request, str(e))
+        return render(request, self.template_name, self.response_dict)
+    else:
+      bookmark = Bookmark()
+
+    form = self.form_class(instance=bookmark)
+
+    self.response_dict[self.ResponseDict.FORM] = form
+
+    return render(request, self.template_name, self.response_dict)
+
+  def post(self, request, *args, **kwargs):
+
+    if request.user and request.user.id:
+      bookmark = Bookmark.objects.get(owner=request.user, id=request.POST['id'])
+
+    else:
+      bookmark = Bookmark.objects.get(owner=None, id=request.POST['id'])
+
+    form = self.form_class(instance=bookmark , data=request.POST)
+
+    if form.is_valid():
+      form.save()
+      messages.info(request, "Bookmark was successfully updated.")
+      query_string = "?id={}".format(bookmark.id)
+      return HttpResponseRedirect(reverse(self.success_page) + query_string)
+
+    else:
+      return render(request, self.template_name, self.response_dict)
