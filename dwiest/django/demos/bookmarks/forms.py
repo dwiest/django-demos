8edbf011ab5d44f7d4feaf0e5c9acc0abc0bceb5
dwiest <dw9494@gmail.com>
2023-02-27 17:47:44 +0000

    BookmarkForm -> QuickBookmarkForm
    Removed unused entries from Fields Enum in QuickBookmarkForm
    
    Added BaseBookmarkForm and BookmarkForm classes.

--- dwiest/django/demos/bookmarks/forms.py
+++ dwiest/django/demos/bookmarks/forms.py
@@ -3,12 +3,9 @@ from enum import Enum
 from ..conf import settings
 from .models import Bookmark
 
-class BookmarkForm(forms.Form):
+class QuickBookmarkForm(forms.Form):
 
   class Fields(str, Enum):
-    ARTICLE_DATE = 'article_date'
-    DESCRIPTION = 'description'
-    NAME = 'name'
     URL = 'url'
 
   class Errors(str, Enum):
@@ -49,3 +46,40 @@ class BookmarkForm(forms.Form):
 
   def save(self):
     self.bookmark.save()
+
+
+class BaseBookmarkForm(forms.ModelForm):
+
+  class Fields(str, Enum):
+    ARTICLE_DATE = 'article_date'
+    DESCRIPTION = 'description'
+    TITLE = 'title'
+    URL = 'url'
+
+class BookmarkForm(BaseBookmarkForm):
+
+  class Meta:
+    model = Bookmark
+    fields =  ['article_date', 'description', 'title', 'url']
+    widgets = {
+      BaseBookmarkForm.Fields.ARTICLE_DATE: forms.TextInput(
+        attrs = {
+          'class': settings.DEMOS_BOOKMARKS_ARTICLE_DATE_CLASS,
+          }
+        ),
+      BaseBookmarkForm.Fields.DESCRIPTION: forms.Textarea(
+        attrs = {
+          'class': settings.DEMOS_BOOKMARKS_DESCRIPTION_CLASS,
+          }
+        ),
+      BaseBookmarkForm.Fields.TITLE: forms.TextInput(
+        attrs = {
+          'class': settings.DEMOS_BOOKMARKS_TITLE_CLASS,
+          }
+        ),
+      BaseBookmarkForm.Fields.URL: forms.TextInput(
+        attrs = {
+          'class': settings.DEMOS_BOOKMARKS_URL_CLASS,
+          }
+        ),
+      }
