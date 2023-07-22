9982a6dafe11b9b1d823f213c355cd542a749417
dwiest <dw9494@gmail.com>
2023-07-22 14:52:46 +0000

    Added DateInput class.
    Added BillToIndexView.setup().
    
    Corrected success_page in BillToEditView.
    
    Additional print statements in BillToEditView.get() and .post().
    
    Append query string when redirecting about BillToEditView.post().
    
    Added InvoiceIndexView.setup().
    
    Retrieve LineItem records in InvoiceView.get() and include them in the
    response dict.
    
    Corrected success_page in InvoiceEditView.
    
    Use modelformset_factory's in InvoiceEditView.get() and .post().
    
    Process the InvoiceForm in InvoiceEditView.post().
    
    Process the LineItem formset in InvoiceEditView.post().
    
    Added class InvoiceRenderView.

--- dwiest/django/demos/invoicer/views.py
+++ dwiest/django/demos/invoicer/views.py
@@ -1,16 +1,22 @@
 from django.contrib import messages
 from django.core.exceptions import ValidationError
 from django.db.models import Q
+from django.forms import modelformset_factory
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, reverse
 from django.template.context import RequestContext
 from django.views.generic import FormView, TemplateView, ListView
 from django.views.generic.base import TemplateResponseMixin
 from enum import Enum
-from datetime import datetime
+from datetime import date, datetime
 from .forms import *
 from .models import BillTo
 from ..conf import settings
+
+
+class DateInput(forms.DateInput):
+  input_type = 'date'
+
 
 
 class BillToIndexView(ListView):
@@ -21,6 +27,15 @@ class BillToIndexView(ListView):
 
   user = None
   template_name = settings.DEMOS_INVOICER_BILL_TO_LIST_TEMPLATE
+
+  def setup(self, request, *args, **kwargs):
+    super().setup(request, *args, **kwargs)
+
+    if request.user.id:
+      self.user = request.user
+    else:
+      self.user = None
+
 
   def get_template_names(self):
     return [self.template_name]
@@ -86,7 +101,7 @@ class BillToEditView(TemplateView):
     FORM = 'form'
 
   form_class = BillToForm
-  success_page = 'demos:invoicer:billto_view'
+  success_page = 'demos:invoicer:bill_to_view'
   template_name = settings.DEMOS_INVOICER_BILL_TO_EDIT_TEMPLATE
 
   def __init__(self, *args, **kwargs):
@@ -109,21 +124,34 @@ class BillToEditView(TemplateView):
     else:
       item = BillTo(owner=request.user)
 
+    print("print item")
+    print(item.name)
     form = self.form_class(instance=item)
 
     self.response_dict[self.ResponseDict.FORM] = form
 
+    print("before render")
     return render(request, self.template_name, self.response_dict)
 
   def post(self, request, *args, **kwargs):
+    print("post")
     if request.POST.get('id'):
       item = BillTo.objects.get(owner=request.user, id=request.POST.get('id'))
     else:
+      print("creating new BillTo")
       item = BillTo(owner=request.user)
+    if item == None:
+      print("Was None?")
+    else:
+      print("before print")
+      print(item)
+
     form = self.form_class(instance=item, data=request.POST)
 
+    print("form instance: {}".format(form.instance.name))
     if form.is_valid():
-      print("is_valid()")
+      print("the form is_valid")
+
       form.save()
       messages.info(request, "Bill-to was successfully updated.")
       query_string = "?id={}".format(form.instance.id)
@@ -131,7 +159,12 @@ class BillToEditView(TemplateView):
     else:
       print("not_valid()")
 
-### HERE
+    query_string = "?id={}".format(form.instance.id)
+    return HttpResponseRedirect(reverse(self.success_page) + query_string)
+
+#    else:
+#      return render(request, self.template_name, self.response_dict)
+
 
 class InvoiceIndexView(ListView):
 
@@ -141,6 +174,14 @@ class InvoiceIndexView(ListView):
 
   user = None
   template_name = settings.DEMOS_INVOICER_INVOICE_LIST_TEMPLATE
+
+  def setup(self, request, *args, **kwargs):
+    super().setup(request, *args, **kwargs)
+
+    if request.user.id:
+      self.user = request.user
+    else:
+      self.user = None
 
   def get_template_names(self):
     return [self.template_name]
@@ -170,6 +211,7 @@ class InvoiceView(TemplateView):
 
   class ResponseDict(str, Enum):
     ITEM = 'invoice'
+    LINE_ITEMS = 'line_items'
 
   template_name = settings.DEMOS_INVOICER_INVOICE_TEMPLATE
 
@@ -183,9 +225,12 @@ class InvoiceView(TemplateView):
       try:
         if request.user and request.user.id:
           item = Invoice.objects.get(owner=request.user, id=id, status=0)
+          line_items = LineItem.objects.filter(owner=request.user, invoice_id=id, status=0)
         else:
+          print("No invoice: {}".format(id))
           item = Invoice.objects.get(owner=None, id=id, status=0)
         print(item)
+        print(line_items)
 
       except Exception as e:
         print(str(e))
@@ -196,6 +241,7 @@ class InvoiceView(TemplateView):
       item = Invoice(owner=request.user)
 
     self.response_dict[self.ResponseDict.ITEM] = item 
+    self.response_dict[self.ResponseDict.LINE_ITEMS] = line_items
 
     return render(request, self.template_name, self.response_dict)
 
@@ -204,9 +250,12 @@ class InvoiceEditView(TemplateView):
 
   class ResponseDict(str, Enum):
     FORM = 'form'
+    FORMSET = 'formset'
+    INVOICE = 'invoice'
+    LINE_ITEMS = 'line_items'
 
   form_class = InvoiceForm
-  success_page = 'demos:invoicer:invoice_view'
+  success_page = 'demos:invoicer:invoice_edit'
   template_name = settings.DEMOS_INVOICER_INVOICE_EDIT_TEMPLATE
 
   def __init__(self, *args, **kwargs):
@@ -232,21 +281,115 @@ class InvoiceEditView(TemplateView):
     form = self.form_class(instance=item)
 
     self.response_dict[self.ResponseDict.FORM] = form
+    self.response_dict[self.ResponseDict.INVOICE] = item
+
+    formset = modelformset_factory(LineItem, extra=1,exclude=['id','owner','invoice'],can_delete=True,widgets={'status':forms.NumberInput(attrs={'max':0,'min':-1}),'date':DateInput(attrs={'max': date.today()}),'description':forms.Textarea(attrs={'cols':40,'rows':2}),'quantity':forms.NumberInput(attrs={'min':0.25,'step':0.25})})
+
+    self.response_dict[self.ResponseDict.FORMSET] = formset(queryset=LineItem.objects.filter(invoice=item),initial=[{'date':date.today(),'invoice':item,'description':'initial description'}])
 
     return render(request, self.template_name, self.response_dict)
 
   def post(self, request, *args, **kwargs):
-    if request.POST.get('id'):
-      item = Invoice.objects.get(owner=request.user, id=request.POST.get('id'))
-    else:
-      item = Invoice(owner=request.user)
-    form = self.form_class(instance=item, data=request.POST)
+#    if request.POST.get('id'):
+#      item = Invoice.objects.get(owner=request.user, id=request.POST.get('id'))
+#    else:
+#      item = Invoice(owner=request.user)
+   # form = self.form_class(instance=item, data=request.POST)
 
-    if form.is_valid():
-      print("is_valid()")
-      form.save()
-      messages.info(request, "Invoice was successfully updated.")
-      query_string = "?id={}".format(form.instance.id)
+    invoice_id = request.POST.get('id',1) #FIX<E
+    owner = request.user
+
+    try:
+      invoice = Invoice.objects.get(owner=owner, id=invoice_id)
+    except Exception as e:
+      print("uhoh: {}".format(e))
+      return render(request, self.template_name, self.response_dict)
+
+    invoice_form = self.form_class(instance=invoice, data=request.POST)
+    if invoice_form.is_valid():
+      invoice_form.save();
+    else:
+      print("invoice_form.errors: {}".format(invoice_form.errors))
+
+    line_item_formset = modelformset_factory(
+      LineItem,
+      exclude=['id','owner','invoice'],
+      can_delete=True,
+      widgets={
+        'status':forms.NumberInput(attrs={'max':0,'min':-1}),
+        'date':DateInput(attrs={'max': date.today()}),
+        'description':forms.Textarea(attrs={'cols':40,'rows':2}),
+        'quantity':forms.NumberInput(attrs={'min':0.25,'step':0.25})
+        }
+      )
+
+    print("POST: {}".format(request.POST))
+    formset = line_item_formset(request.POST,initial=[{'date':date.today(),'description':'initial description'}])
+    #,queryset=LineItem.objects.filter(owner=request.user,invoice=invoice))
+
+    if formset.is_valid():
+      print("Formset: is_valid()")
+      line_items = formset.save(commit=False)
+      for line_item in line_items:
+        line_item.invoice = invoice
+        print("Formset: save()")
+        line_item.save()
+      for line_item in formset.deleted_objects:
+        print("Formset: delete()")
+        line_item.delete()
+
+      print("Formset: {} changed_objects, {} deleted_objects, {} new_objects".format(formset.changed_objects, formset.deleted_objects, formset.new_objects))
+      if formset.changed_objects or formset.deleted_objects or formset.new_objects:
+        messages.info(request, "Invoice updated: {} added, {} changed, {} deleted".format(len(formset.new_objects),len(formset.changed_objects),len(formset.deleted_objects)))
+      else:
+        messages.info(request, "No changes were made.")
+      query_string = "?id={}".format(invoice_id)
       return HttpResponseRedirect(reverse(self.success_page) + query_string)
     else:
-      print("not_valid()")
+      print("Formset not valid")
+      print(formset.errors)
+      print("template_name: {}".format(self.template_name))
+      messages.error(request, "Please fix the errors shown below.")
+
+      self.response_dict[self.ResponseDict.FORM] = self.form_class(instance=invoice)
+      self.response_dict[self.ResponseDict.INVOICE] = invoice
+      self.response_dict[self.ResponseDict.FORMSET] = formset
+      return render(request, self.template_name, self.response_dict)
+
+
+class InvoiceRenderView(TemplateView):
+  class ResponseDict(str, Enum):
+    ITEM = 'invoice'
+    LINE_ITEMS = 'line_items'
+
+  template_name = settings.DEMOS_INVOICER_INVOICE_RENDER_TEMPLATE
+
+  def __init__(self, *args, **kwargs):
+    self.response_dict = {}
+
+  def get(self, request, *args, **kwargs):
+    id = request.GET.get('id')
+
+    if id:
+      try:
+        if request.user and request.user.id:
+          item = Invoice.objects.get(owner=request.user, id=id, status=0)
+          line_items = LineItem.objects.filter(owner=request.user, invoice_id=id, status=0)
+        else:
+          item = Invoice.objects.get(owner=None, id=id, status=0)
+        print(item)
+
+      except Exception as e:
+        print(str(e))
+        messages.error(request, str(e))
+        return render(request, self.template_name, self.response_dict)
+    else:
+      print("Shouldn't be here")
+      items = []
+      line_items = []
+
+    self.response_dict[self.ResponseDict.ITEM] = item
+    self.response_dict[self.ResponseDict.LINE_ITEMS] = line_items
+
+    return render(request, self.template_name, self.response_dict)
+
