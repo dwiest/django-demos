ceab023bb28168955ed58fc9bd3930b9b7535ff9
dwiest <dw9494@gmail.com>
2023-07-22 14:32:56 +0000

    Added abstract base classes that extend Model to provide common attributes:   - BaseModel   - NamedModel   - OwnedModel   - Address   - BillTo
    Updated attributes for Invoice and LineItem.

--- dwiest/django/demos/invoicer/models.py
+++ dwiest/django/demos/invoicer/models.py
@@ -1,21 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
+from datetime import timedelta
 
-class BillTo(models.Model):
+class BaseModel(models.Model):
   class Meta:
-    ordering = ['-created_at', '-id']
-
-  owner = models.ForeignKey(
-    User,
-    on_delete=models.PROTECT,
-    blank = True,
-    null = True,
-    )
-
-  name = models.CharField(
-    max_length=255,
-    unique=True,
-    )
+    abstract = True
 
   created_at = models.DateTimeField(
     auto_now_add=True,
@@ -32,9 +21,9 @@ class BillTo(models.Model):
     )
 
 
-class Invoice(models.Model):
+class OwnedModel(models.Model):
   class Meta:
-    ordering = ['-created_at', '-id']
+    abstract = True
 
   owner = models.ForeignKey(
     User,
@@ -43,9 +32,49 @@ class Invoice(models.Model):
     null = True,
     )
 
+
+class NamedModel(models.Model):
+  class Meta:
+    abstract = True
+
   name = models.CharField(
     max_length=255,
     unique=True,
+    )
+
+  description = models.CharField(max_length=255)
+
+
+class Address(BaseModel, OwnedModel, NamedModel, models.Model):
+  class Meta:
+    abstract = True
+#    ordering = ['-created_at', '-id']
+
+  line_1 = models.CharField(max_length=255)
+  line_2 = models.CharField(max_length=255)
+  city = models.CharField(max_length=255)
+  state = models.CharField(max_length=255)
+  #province = models.ChoiceField()
+  #country = models.ChoiceField()
+
+
+class BillTo(Address):
+  def __init__(self, *args, **kwargs):
+    print("BillTo.__init__()")
+    print(kwargs)
+    super().__init__(*args, **kwargs)
+
+  def __str__(self):
+    return self.name
+
+
+class Invoice(BaseModel, OwnedModel, NamedModel):
+
+  bill_to = models.ForeignKey(
+    BillTo,
+    on_delete=models.CASCADE,
+    blank = True,
+    null = True,
     )
 
   invoice_date = models.DateField(
@@ -53,51 +82,77 @@ class Invoice(models.Model):
     editable=True,
     )
 
-  created_at = models.DateTimeField(
-    auto_now_add=True,
-    editable=False,
-    )
 
-  last_modified = models.DateTimeField(
-    auto_now=True,
-    editable=False,
-    )
+  def total(self):
+    if not hasattr(self,'_total'):
+      print("{}.total(): not cached".format(self.__class__.__name__))
+      amount = 0
+      items = []
+      try:
+        items = LineItem.objects.filter(owner=49,invoice=1)
+      except Exception as e:
+        print(e)
+      for item in items:
+        print("amount: {}".format(item.total()))
+        amount += item.total()
+      self._total = amount
+    else:
+      print("{}.total(): cached".format(self.__class__.__name__))
 
-  status = models.IntegerField(
-    default=0,
-    )
+    print("total: {}".format(self._total))
+    return self._total
 
 
-class LineItem(models.Model):
-  class Meta:
-    ordering = ['-created_at', '-id']
+  def due_date(self):
+    terms = 30
+    if hasattr(self,'_due_date') == False:
+      time_delta = timedelta(days=terms)
+      self._due_date = self.invoice_date + time_delta
 
-  owner = models.ForeignKey(
-    User,
-    on_delete=models.PROTECT,
-    blank = True,
-    null = True,
+    print("{}.terms {}".format(self.__class__.__name__, terms))
+    print("{}.invoice_date {}".format(self.__class__.__name__, self.invoice_date))
+    print("{}.due_date {}".format(self.__class__.__name__, self._due_date))
+    return self._due_date
+
+
+class LineItem(BaseModel, OwnedModel):
+#  class Meta:
+#    ordering = ['-created_at', '-id']
+
+  invoice = models.ForeignKey(
+    Invoice,
+    on_delete=models.CASCADE,
+    blank = False,
+    null = False,
     )
 
   date = models.DateField(
-    auto_now_add=True,
+    #auto_now_add=True, # doesn't show on form
+    #blank = True,
+    #null = True,
     editable=True,
     )
 
-  description = models.TextField(
-    null=False,
+  description = models.CharField(max_length=255)
+  rate = models.IntegerField(default=80)
+
+  quantity = models.DecimalField(
+    default        = 1,
+    max_digits     = 4,
+    decimal_places = 2
     )
 
-  created_at = models.DateTimeField(
-    auto_now_add=True,
-    editable=False,
-    )
+  def total(self):
+    return self.quantity * self.rate
 
-  last_modified = models.DateTimeField(
-    auto_now=True,
-    editable=False,
-    )
 
-  status = models.IntegerField(
-    default=0,
-    )
+
+class Address(BaseModel, OwnedModel, NamedModel, models.Model):
+  class Meta:
+    abstract = True
+#    ordering = ['-created_at', '-id']
+
+  line_1 = models.CharField(max_length=255)
+  line_2 = models.CharField(max_length=255)
+  city = models.CharField(max_length=255)
+  state = models.CharField(max_length=255)
