573dcf0ac78bfcbcfb6907762da22773c77c4100
dwiest <dw9494@gmail.com>
2025-04-26 23:34:40 +0000

    Don't hardcode the owner and invoice number.

--- dwiest/django/demos/invoicer/models.py
+++ dwiest/django/demos/invoicer/models.py
@@ -91,7 +91,7 @@ class Invoice(BaseModel, OwnedModel, NamedModel):
       amount = 0
       items = []
       try:
-        items = LineItem.objects.filter(owner=49,invoice=5) #FIXME
+        items = LineItem.objects.filter(owner=self.owner, invoice=self.id)
       except Exception as e:
         print(e)
       for item in items:
